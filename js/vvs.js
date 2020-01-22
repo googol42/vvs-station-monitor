a3471ed0c75c548d6af1fa102cc8d701240917c6
Andreas P <andreaspreikschat@posteo.de>
2020-01-22 14:04:41 +0100

    Feature: use bootstrap and general improvements

--- js/vvs.js
+++ js/vvs.js
@@ -1,16 +1,37 @@
-/* Each entry contains an array. The array must be structured like this:
+/*
+ * Each entry contains an array. The array must be structured like this:
+ *
  *   [id, destination array, ignore array]
  *
  * The 'id' is the station's id you want to appear on the monitor.
  *
- * The 'destination array' is an array with the direction names. Note that
- * each lines can have multiple destination (e. g. at nights). The monitor
- * will log errors for all destination which are not included in the array.
- * This is done to allow noticing configuration errors.
+ * The 'destination array' is an array with the direction names.
+ * All names given in this list, will be displayed. '' means, that all
+ * directions of all lines will be displayed.  Note that a line can have
+ * multiple destination (e. g. at nights). The monitor will log errors
+ * for all destination which are not included in the array. This is done
+ * to allow noticing configuration errors.
  *
  * If you are sure that you don't want to include a destination included
  * the name in the 'ignore array'. Note that [''] means that all stations
  * are ignored.
+ *
+ * Example:
+ *
+ *      ['5000008', ['Vaihingen', 'Vaihingen Bf'], ['']],
+ *
+ * Display only the lines with direction 'Vahingen' and 'Vaihingen Bf'. Ignore
+ * all other destinations (indicated by '').
+ *
+ * An other example:
+ *
+ *      ['5006009', ['Mineralbäder', 'Fellbach', 'Heslach Vogelrain'], ['Vaihingen', 'Vaihingen Bf']],
+ *
+ * Display everything which has the direction 'Mineralbäder', 'Fellbach'
+ * and 'Helsach Vogelrain'. All connections to 'Vaihingen', 'Vaihingen Bf'
+ * are ignored. The connection to 'Fellbach Lutherkirche' will appear in
+ * the debug section, because it is not incldue of none of the arrays.
+ *
 */
 var stations = [
     // Engelboldstraße
@@ -20,13 +41,20 @@ var stations = [
     // Im Elsental
     ['5002578', ['Rohr Mitte', 'Leinfelden'], ['']],
     // Österfeld
-    ['5006027', [''], ['']]
+    ['5006027', [''], []],
+    // Reichenbach (F)
+    ['5004224', [''], []],
 ];
 
 /*
- * Miliseconds to wait before the screen start to fade.
+ * Seconds to wait before the screen start to fade.
  */
-var delayBeforeFade = 30 * 1000;
+var delayBeforeFade = 10;
+
+/*
+ * Updates all stations/connnections all x seconds
+ */
+var updateAll = 60;
 
 function init() {
     if ('ontouchstart' in document.documentElement) {
@@ -42,8 +70,15 @@ function addAllStations() {
       var stationDiv = document.createElement('div');
       stationDiv.id = stations[index][0];
       stationDiv.classList.add('station');
+      stationDiv.classList.add('col-sm-6');
       var stationNameDiv = document.createElement('div');
-      stationNameDiv.classList.add('stationName');
+      stationNameDiv.classList.add('font-weight-bold');
+      stationNameDiv.classList.add('h5');
+      stationNameDiv.classList.add('bg-primary');
+      stationNameDiv.classList.add('rounded');
+      stationNameDiv.classList.add('p-1');
+      stationNameDiv.classList.add('pl-2');
+      stationNameDiv.classList.add('mt-2');
       stationNameDiv.id = "stationName" + stations[index][0];
       stationDiv.appendChild(stationNameDiv);
       var connectionsDiv = document.createElement('div');
@@ -70,27 +105,26 @@ function updateTimeToUpdate(time) {
 }
 
 function setUpdateTimeToUpdateText(time) {
-        var text = 'Aktualisiere...';
-        if (!document.getElementById('next-update-icon').classList.contains('spin')) {
-            text = "Aktualisierung in " + time / 1000 + " s";
+        var text = "Aktualisierung in " + time / 1000 + " s";
+        if (time === 0) {
+            document.getElementById('next-update-icon').classList.remove('invisible');
+            text = 'Aktualisiere...';
+        } else {
+            document.getElementById('next-update-icon').classList.add('invisible');
         }
         document.getElementById('next-update').innerText = text;
 }
 
 function updateStations(withTimeout) {
     if (withTimeout) {
-        var time = 120 * 1000;
+        var time = updateAll * 1000;
         setTimeout(function() {updateStations(true);}, time);
         updateTimeToUpdate(time);
     }
     for (var index = 0; index < stations.length; index++) {
         updateStation(stations[index][0], stations[index][1], stations[index][2]);
     }
-    document.getElementById('next-update-icon').classList.add('spin');
     setUpdateTimeToUpdateText(0);
-    setTimeout(function() {
-        document.getElementById('next-update-icon').classList.remove('spin');
-    }, 1000);
 }
 
 function updateStation(stationId, directions, ignoreDirections) {
@@ -114,7 +148,7 @@ function addConnections(stationId, directions, ignoreDirections, connections) {
     var numberOfStationsAdded = 0;
     for (var i = 0; i < 10000; i++) {
         var connection = JSON.parse(connections)[i];
-        stationDiv.innerText = connection['stopName'];
+        stationDiv.innerText = "ab '" + connection['stopName'] + "'";
         if (!arrayContains(connection['direction'], directions) ) {
             if (!arrayContains(connection['direction'], ignoreDirections)) {
                 console.log('Connection excluded', connection['direction'], directions);
@@ -129,10 +163,10 @@ function addConnections(stationId, directions, ignoreDirections, connections) {
             continue;
         }
         var connectionDiv = document.createElement('div');
-        connectionDiv.classList.add('connection');
 
         var timeSpan = document.createElement("span");
         timeSpan.classList.add('time-span');
+        timeSpan.classList.add('pl-2');
         connectionDiv.appendChild(timeSpan);
         timeSpan.appendChild(getTime(connection));
         timeSpan.appendChild(getDelay(connection));
@@ -163,7 +197,8 @@ function getDelay(connection) {
     var delaySpan = document.createElement("span");
     var delay = connection['delay'];
     if (parseInt(delay) != 0) {
-        delaySpan.classList.add('delay');
+        delaySpan.classList.add('text-danger');
+        delaySpan.classList.add('font-weight-bold');
         delaySpan.innerText = " +" + delay;
     }
     return delaySpan;
@@ -180,12 +215,16 @@ function addDirection(connection) {
 
     var number_ = connection['number'];
     if (number_.startsWith('S')) {
-        image.src = 'https://www.vvs.de/typo3conf/ext/vvs_efa_monitor/Resources/Public/Images/icon.sbahn.20.png';
+        image.src = 'https://www.ssb-ag.de/typo3conf/ext/mq_efa_ssb/Resources/Public/Icons/sbahn.svg';
     } else if (number_.startsWith('U')) {
-        image.src = 'https://www.vvs.de/typo3conf/ext/vvs_efa_monitor/Resources/Public/Images/icon.ubahn.20.png'
+        image.src = 'https://www.ssb-ag.de/typo3conf/ext/mq_efa_ssb/Resources/Public/Icons/ubahn.svg';
+    // is R ever returned?
+    } else if (number_.startsWith('R')) {
+        image.src = 'https://www.ssb-ag.de/typo3conf/ext/mq_efa_ssb/Resources/Public/Icons/rbahn.svg';
     } else {
-        image.src = 'https://www.vvs.de/typo3conf/ext/vvs_efa_monitor/Resources/Public/Images/icon.bus.20.png';
+        image.src = 'https://www.ssb-ag.de/typo3conf/ext/mq_efa_ssb/Resources/Public/Icons/bus.svg';
     }
+    image.style.height = '20px';
     arrowP.innerText = " → ";
     directionP.innerText = " " + number_ + ": " + connection['direction'];
     return root;
@@ -202,12 +241,12 @@ function fadeIn() {
 }
 
 function addTouchListener() {
-    var timeout = setTimeout(fadeOut, delayBeforeFade);
+    var timeout = setTimeout(fadeOut, delayBeforeFade * 1000);
     window.addEventListener('touchstart', function() {
         clearTimeout(timeout);
         updateStations(false);
         fadeIn();
-        timeout = setTimeout(fadeOut, delayBeforeFade);
+        timeout = setTimeout(fadeOut, delayBeforeFade * 1000);
     });
 }
 
